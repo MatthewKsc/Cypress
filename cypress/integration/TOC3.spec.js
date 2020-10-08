ce65f7f18e6163ac845a63b531ce265399cb265c
MatthewKsc <ksciukmateusz@gmail.com>
2020-10-08 20:39:35 +0200

    Delete article test

--- cypress/integration/TOC3.spec.js
+++
@@ -1,3 +0,0 @@
-/*
-PreRequest: In our account should be at least one article
- */
---
+++ cypress/integration/ValidData/TOC3.spec.js
@@ -0,0 +1,37 @@
+/*
+PreRequest: In our account should be at least one article
+ */
+
+describe('Delete article', function (){
+
+    before('Login', function (){
+        cy.SingIn();
+    });
+
+    it('delete article', function (){
+        cy.get('ul.navbar-nav')
+            .children()
+            .contains('Matthewksc')
+            .should('be.visible')
+            .click();
+
+        cy.get('.article-preview')
+            .first()
+            .should('be.visible')
+            .click();
+
+        cy.location('hash').should('contain','#/article');
+
+        cy.get('.btn-outline-danger')
+            .contains('Delete Article')
+            .should('be.visible')
+            .click();
+
+        cy.contains('Your Feed')
+            .should('be.visible');
+    });
+
+    after('Logout', function (){
+        cy.SingOut();
+    });
+})
